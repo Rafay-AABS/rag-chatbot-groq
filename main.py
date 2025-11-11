9ede95020e68de4bd7a0b41c79d45469df3bc20d
Rafay-AABS <abdur.rafay@artificialautomationbizsol.com>
2025-11-11 21:04:22 +0500

    Added upload PDF functionality

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 @app.get("/")
 def home():
     return {"message":"RAG Chatbot API is running!"}
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(status_code=400, detail="Please upload a PDF file.")
+    
+    file_id = str(uuid.uuid4())
+    file_path = os.path.join(UPLOAD_DIR, "f{file_id}.pdf")
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+    
+    pages = extract_text_from_pdf(file_path)
+    return {"document_id": file_id, "pages_extracted": len(pages)}
